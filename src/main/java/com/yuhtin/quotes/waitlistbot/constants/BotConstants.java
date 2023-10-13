ab5ca57c928c80b4793669591791bb1ec1e03def
Davi Duarte <rio.daviduarte@gmail.com>
2023-10-13 09:36:58 -0300

    feat: add mongouri to constants

--- src/main/java/com/yuhtin/quotes/waitlistbot/constants/BotConstants.java
+++ src/main/java/com/yuhtin/quotes/waitlistbot/constants/BotConstants.java
@@ -2,6 +2,7 @@ package com.yuhtin.quotes.waitlistbot.constants;
 
 public class BotConstants {
 
+    public  static final String MONGO_URI = "mongodb+srv://$login$@$address$/test?retryWrites=true&w=majority";
     public static final String WEBHOOK_LINK = "";
     public static final long CHANNEL_ID = 0L;
 
