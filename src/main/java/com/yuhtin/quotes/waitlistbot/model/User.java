9fb5c8bc585d89f1bc0f77ee11cfed3151a01c49
Davi Duarte <rio.daviduarte@gmail.com>
2023-10-13 09:37:33 -0300

    feat: user model

---
+++ src/main/java/com/yuhtin/quotes/waitlistbot/model/User.java
@@ -0,0 +1,22 @@
+package com.yuhtin.quotes.waitlistbot.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * @author <a href="https://github.com/Yuhtin">Yuhtin</a>
+ */
+@Data
+@Builder
+@Accessors(fluent = true)
+public class User {
+
+    private final String memberId;
+    private final String email;
+    private final long creationDate;
+
+    private String discordName;
+    private int referrals;
+
+}
