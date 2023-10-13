7b00072555f94b3ddc3a55f0d6b1bb651883e38f
Davi Duarte <rio.daviduarte@gmail.com>
2023-10-13 11:35:03 -0300

    fix: mongo uri

--- src/main/java/com/yuhtin/quotes/waitlistbot/constants/BotConstants.java
+++ src/main/java/com/yuhtin/quotes/waitlistbot/constants/BotConstants.java
@@ -2,8 +2,8 @@ package com.yuhtin.quotes.waitlistbot.constants;
 
 public class BotConstants {
 
-    public  static final String MONGO_URI = "mongodb+srv://$login$@$address$/test?retryWrites=true&w=majority";
+    public static final String MONGO_URI = "mongodb://$login$@$address$/admin?retryWrites=true&w=majority&appName=mongosh+1.5.4";
     public static final String WEBHOOK_LINK = "";
-    public static final long CHANNEL_ID = 0L;
+    public static final String ENTERPRISE_ICON_URL = "https://www.soba.xyz/img/icons/favicon.ab6fbc.svg";
 
 }
