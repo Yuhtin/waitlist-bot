993caf2ae657766791ad224c42ddb47e1701ef94
Davi Duarte <rio.daviduarte@gmail.com>
2023-11-17 21:47:24 -0300

    feat: add email check using regex

--- src/main/java/com/yuhtin/quotes/waitlistbot/util/DiscordUtil.java
+++ src/main/java/com/yuhtin/quotes/waitlistbot/util/DiscordUtil.java
@@ -5,8 +5,11 @@ package com.yuhtin.quotes.waitlistbot.util;
  */
 public final class DiscordUtil {
 
+    private static final String EMAIL_REGEX = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+
     public static String normalizeDiscordName(String discord) {
         if (discord == null || discord.isBlank()) return null;
+        if (discord.matches(EMAIL_REGEX)) return null;
 
         String normalized = discord.contains("#") ? discord.split("#")[0] : discord;
         return normalized.toLowerCase();
